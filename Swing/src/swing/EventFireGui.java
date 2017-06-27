2efa2889622729052032f7f251183f85f8082605
freenham <freenham@gmail.com>
2017-06-28 03:03:08 +0900

    새벽

--- Swing/src/swing/EventFireGui.java
+++ Swing/src/swing/EventFireGui.java
@@ -3,8 +3,13 @@ package swing;
 import java.awt.Container;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class EventFireGui extends JFrame {
 	private static final long serialVersionUID = -711163588504124217L;
@@ -17,9 +22,32 @@ public class EventFireGui extends JFrame {
 		Container contentPane = this.getContentPane();
 		JPanel pane = new JPanel();
 		JButton buttonStart = new JButton("Start");
+		JTextField textPeroid = new JTextField(5);
+		JLabel labelPeriod = new JLabel("Input period: ");
+		JCheckBox checkboxIsRandom = new JCheckBox("swing 테스트중");
+		
+		checkboxIsRandom.addChangeListener(new ChangeListener() {
+
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				if(((JCheckBox)e.getSource()).isSelected()) {
+					textPeroid.setText("Random");
+					textPeroid.setEnabled(false);
+		}
+				else {
+					textPeroid.setText("");
+					textPeroid.setEnabled(true);
+				}
+			}
+			
+		});
 		buttonStart.setMnemonic('S');
 		
 		pane.add(buttonStart);
+		
+		pane.add(labelPeriod);
+		pane.add(textPeroid);
+		pane.add(checkboxIsRandom);
 		contentPane.add(pane);
 		
 		
